21c31453e555ed9cd609fee37093f3feec2833cb
SidxCoder <connectosid@gmail.com>
2024-12-20 12:37:36 +0530

    changes in backend

--- Backend/src/infrastructure/env.js
+++ Backend/src/infrastructure/env.js
@@ -6,6 +6,8 @@
    MONGO_URL: process.env.MONGO_URL || "",
    SECRET_KEY: process.env.SECRET_KEY || "",
    AWS_ACCESS_ID: process.env.AWS_ACCESS_ID || "",
-   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || ""
+   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || "",
+   MAILER_ID: process.env.MAILER_ID||"",
+   MAILER_PASS: process.env.MAILER_PASS,
  }
 export default env
